a22e01555b9420c766c96ea2a4f8fa36117afa63
levtus <61996958+levtus@users.noreply.github.com>
2023-04-27 11:06:03 -0500

    Add Color Values!

--- gpaGrapher.py
+++ gpaGrapher.py
@@ -1,58 +1,56 @@
 # To Install Package via pip: python3 -m pip install [package name]
 
 # Import Packages
-from matplotlib.axis import XAxis
 import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-from matplotlib.colors import is_color_like
 import matplotlib.ticker as ticker
+from matplotlib.colors import is_color_like
+import numpy as np
 from numpy import mean 
 
 # Set Constants
-allowed = set("0123456789., ") # Allowed Characters
+allowed = set('0123456789., ') # Allowed Characters
 gpaValues = np.array([])
 gpa = np.array([])
 semesters = np.array([])
 weighting = ''
 mode = ''
 
-# Set Data
+# Get Data Mode
 while True:
-    mode = input('Would you like to graph your semester GPA or your cumulative GPA? (semester/cumulative): ')
+    mode = input('\033[1;36m Would you like to graph your semester GPA or your cumulative GPA? (semester/cumulative): ')
     if mode.lower() == 'semester':
-        print('Great, semester GPA it is!')
+        print('\033[1;32m Great, semester GPA it is!')
         break
     elif mode.lower() == 'cumulative':
-        print('Great, cumulative GPA it is!')
+        print('\033[1;32m Great, cumulative GPA it is!')
         break
     else: 
-        print('Sorry, your input was\'nt understood. Please try again.')
+        print('\033[1;31m Sorry, your input was\'nt understood. Please try again.') 
 
 while True:
-    weighting = input('Would you like to graph your weighted GPA or your unweighted GPA? (weighted/unweighted): ')
+    weighting = input('\033[1;36m Would you like to graph your weighted GPA or your unweighted GPA? (weighted/unweighted): ')
     if weighting.lower() == 'weighted':
-        print('Great, weighted GPA will be graphed!')
+        print('\033[1;32m Great, weighted GPA will be graphed!')
         break
     elif weighting.lower() == 'unweighted':
-        print('Great, unweighted GPA will be graphed!')
+        print('\033[1;32m Great, unweighted GPA will be graphed!')
         break
     else: 
-        print('Sorry, your input wasn\'t understood. Please try again.')
+        print('\033[1;31m Sorry, your input wasn\'t understood. Please try again.')
 
 while True:
-    color = input('What color would you like the series to take? (red, orange, yellow, green, blue, purple, pink, brown, gray, black, etc.): ')
+    color = input('\033[1;36m What color would you like the series to take? (red, orange, yellow, green, blue, purple, pink, brown, gray, black, etc.): ')
     if is_color_like(color) == True:
-        print('Great, the color will be ' + color + '!')
+        print('\033[1;32m Great, the color will be ' + color + '!')
         break
-    print('That doesn\'t seem to be a valid color. Please Try Again: ')
+    print('\033[1;31m That doesn\'t seem to be a valid color. Please Try Again.')
 
 while True: 
-    gpaString = input('Input your space-separated GPA values: ')
+    gpaString = input('\033[1;36m Input your space-separated GPA values: ')
     if gpaString and allowed.issuperset(gpaString):
         gpaValues = gpaString.split()
         break
-    print('Invalid characters entered! Please Try Again: ')
+    print('\033[1;31m Invalid characters entered! Please Try Again: ')
 
 
 if mode.lower() == 'semester':
@@ -85,7 +83,7 @@ else:
     plt.legend(['GPA', 'United States Average GPA'])
 
 # Set Plot Bounds
-plt.ylim(0, 5)
+plt.ylim(0, 5.5)
 plt.xlim(1, len(gpaValues))
 
 # Add Grid Lines of Opacity 0.3
