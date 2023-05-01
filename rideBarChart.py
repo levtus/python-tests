036adefe72b1027355afeccea0d8edef6caeb070
levtus <61996958+levtus@users.noreply.github.com>
2023-05-01 13:03:27 -0500

    Create rideBarChart.py

---
+++ rideBarChart.py
@@ -0,0 +1,8 @@
+# Import Packages
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from matplotlib.colors import is_color_like
+import numpy as np
+from numpy import mean 
+
+plt.bar()
